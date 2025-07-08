6fa37d5b464ac7a59b5919621f5ee9b0d53a89b0
dolgoonnn <dolgoon.da@gmail.com>
2025-07-08 17:31:57 +0800

    added example, constants and upgraded flip page

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
+import { API_BASE_URL } from "./config";
 
-const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:3001/api";
 
 export const api = axios.create({
   baseURL: API_BASE_URL,
