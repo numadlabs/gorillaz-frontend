fb91f178719493c4661116319dfe3fe44e1a8f02
Munkh-Od <mnkhod.dev@gmail.com>
2025-07-10 23:54:21 +0800

    Added Manual Metamask Disconnect Bug Fix

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,6 +1,20 @@
-import React from "react";
+"use client"
+import { useRouter } from "next/navigation";
+import React, { useEffect } from "react";
+import { useAccount } from "wagmi";
 
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
+  const router = useRouter();
+  const { address } = useAccount();
+
+  useEffect(() => {
+    if(!address){
+      localStorage.removeItem("gorillaz_token")
+      router.push("/");
+    }
+  }, [])
+  
+
   return <div>{children}</div>;
 };
 
